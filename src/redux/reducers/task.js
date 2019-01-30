3f7f138211a0ecf0d9ff3874d45ad977239b746f
Henrique Kuwai <henrique.kuwai@infracommerce.com.br>
2019-01-30 14:03:30 -0200

    Finalizando dispatcher (reducer)

--- src/redux/reducers/task.js
+++ src/redux/reducers/task.js
@@ -10,7 +10,26 @@ const initialState = [
 ];
 
 export default (state = initialState, action) => {
+    const scopedState = state;
     switch (action.type) {
+        case 'ADD_TODO':
+            return [].concat(state, [{
+                title: action.payload,
+                isDone: false
+            }]);
+
+        case 'REMOVE_TODO':
+            scopedState.splice(action.payload, 1);
+            return [].concat(scopedState);
+
+        case 'MARK_TODO_AS_DONE':
+            scopedState[action.payload].isDone = true;
+            return [].concat(scopedState);
+
+        case 'UNMARK_TODO_AS_DONE':
+            scopedState[action.payload].isDone = false;
+            return [].concat(scopedState);
+
         default:
             return state;
     }
